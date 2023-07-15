a46f021c8b24869f62563778cdc87393bf76723a
neil0904 <99093936+neil0904@users.noreply.github.com>
2023-07-15 23:19:27 +0500

    Add files via upload
    This is a mini project on reactjs, vitejs, and nodejs. I made it as a beginner.

---
+++ App.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import taskLogo from './Tasktick.png';
+import './style.css';
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: newItem, completed: false }
+    ]);
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+   
+    <div className='mainDiv'>
+    <div>
+      <img src={taskLogo}/>
+    </div>
+      <form onSubmit={handleSubmit} className="newItemForm">
+        <div className="rowForm">
+          <label htmlFor="newItem">Add a new task</label>
+          </div>
+          <div>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="newItem"
+          />
+        </div>
+        <button type="submit" className="btn">
+          Add
+        </button>
+      </form>
+      <h1 className="todoList">Todo List</h1>
+      <ol className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+              />
+              {todo.title}
+            </label>
+            <button
+              onClick={() => deleteTodo(todo.id)}
+              className="btn">
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+      </div>
+    </>
+  );
+}
